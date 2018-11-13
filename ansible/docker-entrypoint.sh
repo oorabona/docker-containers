1f048988e87e3b08a322095b4115f80fb87bfccc
Olivier ORABONA <oorabona@users.noreply.github.com>
2018-11-13 10:14:07 +0100

    feat(ansible) initial commit :rocket:

---
+++ ansible/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# We are assuming that we are in WORKDIR
+HOST_UID=$(stat -c %u .)
+HOST_GID=$(stat -c %g .)
+
+# And we change ansible UID/GID according to the host UID/GID
+sudo usermod -u $HOST_UID ansible
+sudo groupmod -g $HOST_GID ansible
+
+case "$1" in
+  playbook|vault )
+    shift
+    ansible-${1} $@
+    ;;
+  run-script )
+    shift
+    . "$@"
+    ;;
+  * )
+    exec "$@"
+esac
+
+if [ ! -z ${DEBUG} ]
+then
+  read -p "DEBUG environment variable set to ${DEBUG} ... Press <ENTER> key to finish."
+fi
