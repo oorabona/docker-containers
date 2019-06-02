b1227e21524848255b5b12b74bae68be8f5ac639
Olivier Orabona <oorabona@users.noreply.github.com>
2019-06-03 00:07:33 +0200

    fix(sslh/docker-entrypoint.sh) change --ssl to --tls as per #1

--- sslh/docker-entrypoint.sh
+++ sslh/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEFAULT_CMD="-p ${LISTEN_IP}:${LISTEN_PORT} --ssh ${SSH_HOST}:${SSH_PORT} --ssl ${HTTPS_HOST}:${HTTPS_PORT} --openvpn ${OPENVPN_HOST}:${OPENVPN_PORT}"
+DEFAULT_CMD="-p ${LISTEN_IP}:${LISTEN_PORT} --ssh ${SSH_HOST}:${SSH_PORT} --tls ${HTTPS_HOST}:${HTTPS_PORT} --openvpn ${OPENVPN_HOST}:${OPENVPN_PORT}"
 
 set -e
 
