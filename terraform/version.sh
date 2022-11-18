61fcbb22526ee86f1cc28e88ce98387d1b114919
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-11-18 01:14:30 +0100

    get latest version not version 'latest'

--- terraform/version.sh
+++ terraform/version.sh
@@ -1,7 +1,8 @@
 source "../helpers/docker-tags"
 
 if [ "$1" == "latest" ]; then
-  docker-latest-tag hashicorp/terraform "^latest$"
+  # docker-latest-tag hashicorp/terraform "^latest$"
+  docker-latest-tag hashicorp/terraform "^[0-9]+\.[0-9]+\.[0-9]+$"
 else
   docker-check-tag hashicorp/terraform "^${1}$"
 fi
