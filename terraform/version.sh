b5b34a255b82eb9ac218ea96b97d6d60c55cffbc
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-11-20 17:08:00 +0100

    fix function calls

--- terraform/version.sh
+++ terraform/version.sh
@@ -2,7 +2,7 @@ source "../helpers/docker-tags"
 
 if [ "$1" == "latest" ]; then
   # docker-latest-tag hashicorp/terraform "^latest$"
-  docker-latest-tag hashicorp/terraform "^[0-9]+\.[0-9]+\.[0-9]+$"
+  latest-docker-tag hashicorp/terraform "^[0-9]+\.[0-9]+\.[0-9]+$"
 else
-  docker-check-tag hashicorp/terraform "^${1}$"
+  check-docker-tag hashicorp/terraform "^${1}$"
 fi
