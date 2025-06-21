de310cd126e4db93766cfe3aa76017fc71c594c2
Olivier ORABONA <oorabona@users.noreply.github.com>
2025-06-21 12:58:24 +0200

    fix: use git to check upstream tag

--- ansible/version.sh
+++ ansible/version.sh
@@ -3,15 +3,5 @@ source "../helpers/python-tags"
 if [ "$1" == "latest" ]; then
   get_pypi_latest_version ansible
 else
-  check_git_tag $1  # Validate and return the version passed as argument
+  git-check-tag ansible ansible $1
 fi
-
-check_git_tag() {
-  local version=$1
-  if git ls-remote --tags origin | grep -q "refs/tags/$version$"; then
-    echo $version
-  else
-    echo "Error: Version $version does not exist upstream." >&2
-    exit 1
-  fi
-}
